f75ec9634253aa1308ef9619f2322fcbb32d9eac
hhj <mnbdj12345@gmail.com>
2024-05-06 00:30:55 +0900

    통합된 상태의 review_crawler

--- review_crawler.py
+++ review_crawler.py
@@ -1,3 +1,4 @@
+import crawler
 import json
 import time
 from time import sleep
@@ -8,136 +9,121 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-        
-def time_wait(num, code):
-    try:
-        wait = WebDriverWait(driver, num).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, code)))
-    except:
-        print(code, '태그를 찾지 못하였습니다.')
-        driver.quit()
-    return wait
 
-def review_print():
-    print("후기 크롤링 시작")
-    # css를 찾을때 까지 10초 대기
-    time_wait(10, '#mArticle > div.cont_essential > div:nth-child(1) > div.place_details')
-    driver.find_element(By.XPATH, '//*[@id="mArticle"]/div[1]/div[1]/div[2]/div/div[2]/a[2]').send_keys(Keys.ENTER)
-    more = (driver.find_element(By.CSS_SELECTOR, '#mArticle > div.cont_evaluation > div.evaluation_review > .link_more')).text
-    while(more != "후기 접기"):
-        driver.find_element(By.CSS_SELECTOR,'#mArticle > div.cont_evaluation > div.evaluation_review > .link_more').send_keys(Keys.ENTER)
-        more = (driver.find_element(By.CSS_SELECTOR, '#mArticle > div.cont_evaluation > div.evaluation_review > .link_more')).text
-        print(more)
-        time.sleep(0.3)
-    
-    
-def review_crawler():
-    # 리뷰어 정보
-    review_list = driver.find_elements(By.CSS_SELECTOR, '.evaluation_review > .list_evaluation > li')
-    # 유저 개인 프로필
-    user_profiles = driver.find_elements(By.CSS_SELECTOR, '.evaluation_review > .list_evaluation > li > .profile_info')
+def user_review_crawler():
 
-    # 유저 평균평점, 후기 개수
-    user_info = driver.find_elements(By.CSS_SELECTOR, '.unit_info > .txt_desc')
+    time.sleep(0.2)
 
-    # 후기 생성 날짜
-    review_created = driver.find_elements(By.CSS_SELECTOR, '.unit_info > .time_write')
-    
-    # 유저 이름
-    user_names = driver.find_elements(By.CSS_SELECTOR, '.unit_info > .link_user > .inner_user > .txt_username')
+	# (3) 각 요소들 전체 긁어오기
+    review_list = crawler.driver.find_elements(By.CSS_SELECTOR, '.list_body > .FavoriteEvaluationItem')
+    names = crawler.driver.find_elements(By.CSS_SELECTOR, '.list_body > .FavoriteEvaluationItem > .group_tit > .tit_evaluation > .link_txt')
+    ratings = crawler.driver.find_elements(By.CSS_SELECTOR, '.list_body > .FavoriteEvaluationItem > .rating > .score > em')
+    created_dates = crawler.driver.find_elements(By.CSS_SELECTOR, '.list_body > .FavoriteEvaluationItem > .rating > .num_date')
+    descriptions = crawler.driver.find_elements(By.CSS_SELECTOR, '.list_body > .FavoriteEvaluationItem > .desc_directory')
 
-    # 유저 레벨
-    user_levels = driver.find_elements(By.CSS_SELECTOR, '.unit_info > .link_user > .inner_user > .badge_info > .txt_badge')
+    for index in range(len(review_list)):
+        print(index)
 
-    for i in range(len(review_list)):
-        user_review_cnt_index = i * 2
-        user_review_avg_index = i * 2 + 1
-        print(f"review_list{i} 출력")
+		# 식당 이름
+        restaurant_name = names[index].text
+        names[index].send_keys(Keys.ENTER)
+        print(restaurant_name)
 
-        #유저 아이디
-        user_id = review_list[i].get_attribute('data-userid')
-        print(user_id)
-
-        #유저 이름
-        user_name = user_names[i].text
-        print(user_name)
-
-        #유저 평균 평점
-        user_review_avg = user_info[user_review_avg_index].text
-        print(user_review_avg)
-
-        #유저 레벨
-        user_level = user_levels[i].text
-        print(user_level)
-
-        #유저 리뷰개수
-        user_review_cnt = user_info[user_review_cnt_index].text
-        print(user_review_cnt)
-
-        dict_temp = {
-            'userId': user_id,
-            'userName': user_name,
-            'averageScore': user_review_avg,
-            'level': user_level,
-            'reviewCount': user_review_cnt
-        }
-
-        user_dict['유저 정보'].append(dict_temp)
-        print(f'유저 {user_name} 정보 ...완료')
-
-
-        #리뷰 생성 날짜
-        review_created_date = review_created[i].text
-        print(review_created_date)
-        
-        # 유저 평점 계산
-        style_attr = driver.find_element(By.CSS_SELECTOR, '#mArticle > div.cont_evaluation > div.evaluation_review > ul > li:nth-child(1) > div.star_info > div > span > span').get_attribute('style')
-        width_match = re.search(r'width: (\d+)%', style_attr)
-        width_percentage = int(width_match.group(1))
-        score = round((width_percentage / 100) * 5) # width 값에 따라 점수 계산 (예: 100% -> 5점)
-        print(score)
+		# 별점
+        review_score = ratings[index].text
+        print(review_score)
 
         # 리뷰 내용
-        try:
-            user_description = driver.find_element(By.CSS_SELECTOR, f'#mArticle > div.cont_evaluation > div.evaluation_review > ul > li:nth-child({i+1}) > div.comment_info > p > span').text
-            print(user_description)
-        except:
-            user_description = ''
-            print("리뷰 내용 오류")
+        description = descriptions[index].text
+        print(description)
 
+        # 생성일자
+        created_date = created_dates[index].text
+        print(created_date)
+
+        # dict에 데이터 집어넣기
         dict_temp = {
-            'reviewScore': score,
-            'description': user_description,
-            'normScore' : "normScore", # 추후 추가
-            'userId': user_id,
-            'restaurnatId': "restaurant_id", # 추후 추가
-            #'averageScore': average_score,
-            'level': user_level,
-            'createAt': review_created_date
+            'returantName': restaurant_name,
+            'reviewScore': review_score,
+            'description': description,
+            'createdAt': created_date,
         }
 
-        restaurant_review_dict['식당 리뷰 정보'].append(dict_temp)
-        print(f'{user_name} ...완료')
+        user_reviews_dict[f'{user_name}'].append(dict_temp)
+        print(f'{restaurant_name} ...완료')
 
-        # 유저별 리뷰 모으기
-        #profile_info = user_profiles[i].click()
-        
+def review_crawler():
+    
+    # css를 찾을때 까지 10초 대기
+    crawler.time_wait(10, '.FavoriteOther > .header > .FavoriteOtherMethodType > .ACTIVE > a')
+
+    # (2) 후기 탭 클릭
+    review_tab = crawler.driver.find_element(By.CSS_SELECTOR, '.FavoriteOther > .header > .FavoriteOtherMethodType > .ACTIVE > a')
+    review_tab.send_keys(Keys.ENTER)
+
+    sleep(1)
+
+    # 리뷰 리스트
+    review_list = crawler.driver.find_elements(By.CSS_SELECTOR, '.list_body > .FavoriteEvaluationItem')
+    
+    # 유저 이름
+    global user_name 
+    user_name = crawler.driver.find_elements(By.CSS_SELECTOR, '#info\.other > div.header > div > div.FavoriteOtherProfile > div.wrap_user > strong')
+
+    # dictionary 생성
+    global user_reviews_dict
+    user_reviews_dict = {f'{user_name}': []}
+
+    # 시작시간
+    start = time.time()
+    print('[크롤링 시작...]')
+
+    # 페이지 리스트만큼 크롤링하기
+    page = 1    # 현재 크롤링하는 페이지가 전체에서 몇번째 페이지인지
+    page2 = 0   # 1 ~ 5번째 중 몇번째인지
+    error_cnt = 0
 
 
-url = 'https://place.map.kakao.com/19714690'
-driver = webdriver.Chrome() # 크롬창 숨기기
-driver.get(url)
+    if len(review_list) < 15:
+        user_review_crawler()
+    else:
+        while 1:
+            # 페이지 넘어가며 출력
+            try:
+                page2 += 1
+                print("**", page, "**")
 
-user_dict = {'유저 정보': []}
-restaurant_review_dict = {'식당 리뷰 정보': []}
-# 모든 리뷰 보이게 하기
-review_print()
-time.sleep(3)
-# 가게 별 리뷰 크롤링
-review_crawler()
+                # (7) 페이지 번호 클릭
+                crawler.driver.find_element(By.XPATH, f'//*[@id="info.search.page.no{page2}"]').send_keys(Keys.ENTER)
+                
+                # 주차장 리스트 크롤링
+                user_review_crawler()
 
-with open('data/user_dict.json', 'w', encoding='utf-8') as f:
-    json.dump(user_dict, f, indent=4, ensure_ascii=False)
+                # 해당 페이지 리뷰 리스트
+                review_list = crawler.driver.find_elements(By.CSS_SELECTOR, '.list_body > .FavoriteEvaluationItem')
 
-with open('data/restaurant_review_dict.json', 'w', encoding='utf-8') as f:
-    json.dump(restaurant_review_dict, f, indent=4, ensure_ascii=False)
+                # 한 페이지에 장소 개수가 15개 미만이라면 해당 페이지는 마지막 페이지
+                if len(review_list) < 15:
+                    break
+                # 다음 버튼을 누를 수 없다면 마지막 페이지
+                if not crawler.driver.find_element(By.XPATH, '//*[@id="info.search.page.next"]').is_enabled():
+                    break
+
+                # (8) 다섯번째 페이지까지 왔다면 다음 버튼을 누르고 page2 = 0으로 초기화
+                if page2 % 5 == 0:
+                    crawler.driver.find_element(By.XPATH, '//*[@id="info.search.page.next"]').send_keys(Keys.ENTER)
+                    page2 = 0
+
+                page += 1
+
+            except Exception as e:
+                error_cnt += 1
+                print(e)
+                print('ERROR!' * 3)
+
+                if error_cnt > 5:
+                    break
+
+    print('[데이터 수집 완료]\n소요 시간 :', time.time() - start)
+    
+    return user_reviews_dict
